fd0cadf6bb516e5e638b20f5be58589c53fe06c5
Jonathan East <jeast3113@gmail.com>
2022-08-23 16:12:09 -0400

    changed the flow with the grid and decided to use a display grid concept for css; added grid display with templates for rows and columns; created a variable in css to use in js to formulate the total amount of cells inside a grid container

--- js/script.js
+++ js/script.js
@@ -5,8 +5,7 @@ console.log(body);
 const gridContainerDiv = document.createElement('div');
 gridContainerDiv.classList.add('grid-container');
 body.appendChild(gridContainerDiv); 
-let gridCellRowDiv;
-let gridCellColumnDiv;
+
 counter = 1;
 
 
@@ -15,16 +14,12 @@ counter = 1;
 // with a parameter set by default 16x16 
 // test output with a counter
 function creatingGrid(rowCol = 16) {
-    for(let rows = 0; rows < rowCol; rows++) {
-        gridCellRowDiv = document.createElement('div');
-        gridCellRowDiv.classList.add('grid-cell-row');
-        for(let cols = 0; cols < rowCol; cols++) {
-            gridCellColumnDiv = document.createElement('div');
-            gridCellColumnDiv.classList.add('grid-cell-column');
-            // gridCellColumnDiv.textContent += counter++;
-            gridCellRowDiv.appendChild(gridCellColumnDiv);
-        }
-        gridContainerDiv.appendChild(gridCellRowDiv);
+    let totalCells = rowCol * rowCol
+
+    document.documentElement.style.setProperty("--grid-rowCol", rowCol);
+    for(let cell = 0; cell < totalCells; cell++) {
+        let rowColDiv = document.createElement('div');
+        gridContainerDiv.appendChild(rowColDiv);
     }
 }
 
